5c703c0b74f36a79b50d61a205e24f9ef4b6a843
Maxim Tereshchenko <tereshchenko.xini@gmail.com>
2022-01-28 22:47:59 +0300

    EmptySequence

---
+++ src/main/java/org/example/sequence/base/EmptySequence.java
@@ -0,0 +1,95 @@
+package org.example.sequence.base;
+
+import org.example.accumulator.base.Accumulator;
+
+import java.util.Comparator;
+import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+/**
+ * @author Maxim Tereshchenko
+ */
+public final class EmptySequence<T> implements Sequence<T> {
+
+    @Override
+    public <R> R fold(Accumulator<T, R> accumulator) {
+        return accumulator.onFinish();
+    }
+
+    @Override
+    public Sequence<T> filter(Predicate<T> predicate) {
+        return this;
+    }
+
+    @Override
+    public <R> Sequence<R> map(Function<T, R> mapper) {
+        return new EmptySequence<>();
+    }
+
+    @Override
+    public <R> Sequence<R> flatMap(Function<T, Sequence<R>> mapper) {
+        return new EmptySequence<>();
+    }
+
+    @Override
+    public Sequence<T> distinct() {
+        return this;
+    }
+
+    @Override
+    public Sequence<T> sorted(Comparator<T> comparator) {
+        return this;
+    }
+
+    @Override
+    public Sequence<T> onEach(Consumer<T> consumer) {
+        return this;
+    }
+
+    @Override
+    public Sequence<T> limit(long size) {
+        return this;
+    }
+
+    @Override
+    public Sequence<T> skip(long count) {
+        return this;
+    }
+
+    @Override
+    public void forEach(Consumer<T> consumer) {
+        // empty
+    }
+
+    @Override
+    public Optional<T> min(Comparator<T> comparator) {
+        return Optional.empty();
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
+
+    @Override
+    public Optional<T> findFirst() {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean anyMatch(Predicate<T> predicate) {
+        return false;
+    }
+
+    @Override
+    public boolean allMatch(Predicate<T> predicate) {
+        return true;
+    }
+
+    @Override
+    public boolean noneMatch(Predicate<T> predicate) {
+        return true;
+    }
+}
